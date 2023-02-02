6f7130fa83cd62ec46cb5dc63b31d2c0fab5e812
Carlos3108 <c.silva3108@hotmail.com>
2023-02-01 23:45:44 -0300

    consumindo o endpoint de cambio para converter o valor do livro de acordo com os paremetros informados

--- book-service/src/main/java/com/example/bookservice/controller/BookController.java
+++ book-service/src/main/java/com/example/bookservice/controller/BookController.java
@@ -2,14 +2,17 @@ package com.example.bookservice.controller;
 
 import com.example.bookservice.model.Book;
 import com.example.bookservice.repository.BookRepository;
+import com.example.bookservice.response.Cambio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Date;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("book-service")
@@ -24,9 +27,24 @@ public class BookController {
     @GetMapping(value = "/{id}/{currency}")
     public Book findBook(@PathVariable("id") Long id, @PathVariable("currency") String currency) {
         var book = repository.getById(id);
+
         if (book == null)throw new RuntimeException("Book not Found");
+
+        HashMap <String, String> params = new HashMap<>();
+
+        params.put("amount", book.getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+
+        var response = new RestTemplate()
+                .getForEntity("http://localhost:8000/cambio-service/{amount}/{from}/{to}",
+                Cambio.class, params);
+
+        var cambio = response.getBody();
         var port = environment.getProperty("local.server.port");
+
         book.setEnvironment(port);
+        book.setPrice(cambio.getConvertedValue());
         return book;
     }
 }
